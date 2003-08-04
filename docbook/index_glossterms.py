c6256d7f31a8387323b0757cb9d082107e755f33
Edward Loper <edloper@ldc.upenn.edu>
2003-08-04 21:20:37 +0000

    - Replaced with a much simpler implementation that fixes a bug (it   missed <glossterm>s that spanned multiple lines before)
    svn/trunk@1064

--- docbook/index_glossterms.py
+++ docbook/index_glossterms.py
@@ -10,31 +10,13 @@ if len(sys.argv) != 3:
     print 'Usage: %s <input> <output>' % sys.argv[0]
     sys.exit(1)
 
-# Open the output file for writing.
-outfile = open(sys.argv[2], 'w')
+# Define a regexp that finds glossterms.
+GT_RE = re.compile('<glossterm>(.*?)</glossterm>', re.DOTALL)
 
-PARA_END_RE = re.compile(r'(</para>)\s*')
+# Add an indexterm after each glossterm.
+str = GT_RE.sub(r'<glossterm>\1</glossterm>'+
+                r'<indexterm><primary>\1</primary></indexterm>',
+                open(sys.argv[1]).read())
 
-# We will use re.sub to look for all the glossary terms on each
-# line, using the following regexp and sub function:
-GLOSSTERM_RE = re.compile('<glossterm>(.*?)</glossterm>')
-glossterms = []
-def glossterm_sub(match, glossterms=glossterms):
-    glossterms.append(match.group(1))
-
-for line in open(sys.argv[1]).readlines():
-    # Find all the glossary terms on this line.
-    GLOSSTERM_RE.sub(glossterm_sub, line)
-
-    # If a paragraph ended, then add any accumulated glossary
-    # terms right after it.
-    if glossterms and PARA_END_RE.search(line):
-        indexterms = '\n'
-        for t in glossterms:
-            indexterms += '<indexterm><primary>%s</primary></indexterm>\n' % t
-        line = PARA_END_RE.sub(r'\1'+indexterms, line)
-        glossterms[:] = []
-
-    # Write the line to the output file.
-    outfile.write(line)
-
+# Write to the output file.
+open(sys.argv[2], 'w').write(str)
