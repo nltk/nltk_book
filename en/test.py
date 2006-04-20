a1985e67baa4747f3880c1b96a59e666c67c40cc
Steven Bird <stevenbird1@gmail.com>
2006-04-20 02:56:23 +0000

    fixed several program example errors
    svn/trunk@3097

--- en/test.py
+++ en/test.py
@@ -1,2 +1,5 @@
-import doctest
-doctest.testfile("tag.txt")
+from nltk_lite.corpora import cmudict
+from string import join
+for word, num, pron in cmudict.raw():
+    if pron[-4:] == ('N', 'IH0', 'K', 'S'):
+        print word, "/", ' '.join(pron)
