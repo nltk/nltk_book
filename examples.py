a45f61a683d5426118fabd72a8dce086dcc50c45
Steven Bird <stevenbird1@gmail.com>
2008-08-17 09:33:21 +0000

    Fixed formatting of pylistings; they should only be indented by 3 spaces otherwise the automatically generated python files have unwanted line-initial spaces.
    svn/trunk@6446

--- examples.py
+++ examples.py
@@ -12,19 +12,18 @@ Extract the code samples from a file in restructured text format
 """
 
 import sys
+import warnings
+
 from epydoc.markup.doctest import DoctestColorizer
 PROMPT_RE = DoctestColorizer.PROMPT_RE        
 
-for file in sys.argv:
+for file in sys.argv[1:]:
     in_code = False
     for line in open(file).readlines():
         if PROMPT_RE.match(line):
             in_code = True
             print PROMPT_RE.sub('', line),
+
         elif in_code:
             in_code = False
             print
-
-
-
-
