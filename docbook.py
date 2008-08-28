b22274c5d657e4eb9b09a3df70d986815e360280
Paul Bone <pbone@csse.unimelb.edu.au>
2008-08-28 04:31:15 +0000

    Docbook output: ch00.rst should be a 'preface' rather than 'chapter'.
    svn/trunk@6503

--- docbook.py
+++ docbook.py
@@ -223,7 +223,8 @@ class DocBookTranslator(nodes.NodeVisitor):
 
     def visit_admonition(self, node, name=''):
         # This shouldn't be placed in chapters.
-        if self.doctype == "chapter":
+        if self.doctype == "chapter" or \
+                self.doctype == "preface":
             raise nodes.SkipNode
 
         self.body.append(self.starttag(node, 'note'))
