b289d7dbc369105bb289b89bc3a3bde7e2eec0c9
Steven Bird <stevenbird1@gmail.com>
2008-11-04 03:22:56 +0000

    nltk/util.py * replaced UserDict with dict as base class for OrderedDict
    nltk/probability.py
    * FreqDist and ConditionalFreqDist now supports inequalities,
      e.g. FreqDist(e1) <= FreqDist(e2) if e1 subseteq e2
    
    nltk/book.py
    * fixed typo
    
    nltk/corpus/reader/wordlist.py
    * made "en" the default wordlist
    
    nltk/nltk/misc/*.py 
    * made modules more easy to access externally
    
    doc/en/ch01.rst
    * added inequality example to FreqDist summary table
    
    doc/en/ch02.rst
    * added ConditionalFreqDist summary table
    
    doc/en/ch03.rst
    * reworked discussion of regular expressions; removed material about
      matching raw text in favour of a pipelined approach where we
      use regexps for tokenizing, then use regexps in the resulting
      word sequences.
    * tried to use better-motivated examples throughout
    
    nltk_data/corpora/words.zip
    * much larger list of English words
    
    
    
    svn/trunk@6776

--- rst.py
+++ rst.py
@@ -2,7 +2,7 @@
 #
 # Natural Language Toolkit: Documentation generation script
 #
-# Copyright (C) 2001-2006 NLTK Project
+# Copyright (C) 2001-2008 NLTK Project
 # Author: Edward Loper <edloper@gradient.cis.upenn.edu>
 # URL: <http://nltk.org>
 # For license information, see LICENSE.TXT
