aaa6eb219fc7644b1b4513a11bdb23e71ddfd107
Steven Bird <stevenbird1@gmail.com>
2008-11-25 00:01:53 +0000

    Makefile * Fix for 0.9.6b1 release; remove data from dmg distribution,   complete command lines for syncing to googlecode
    doc/docbook.py
    * reinstated condition for mediaobjects inside tables
    
    nltk/__init__.py
    * changed version to 0.9.6b1
    
    doc/en/ch06.rst
    * removing redundant content
    * updated example to use enumerate instead
      of loop counter
    
    nltk/test/tag.doctest
    * fixed formatting problem that caused doctest to fail
    
    
    
    svn/trunk@6972

--- docbook.py
+++ docbook.py
@@ -807,7 +807,7 @@ class DocBookTranslator(nodes.NodeVisitor):
         # self.body.append('<para>')
 
         # ensure image is contained in a figure so that it is allocated page space
-        if element == 'mediaobject' and not self.figure_tag_stack: # and not self.table_tag_stack:
+        if element == 'mediaobject' and not self.figure_tag_stack and not self.table_tag_stack:
             self.body.append('<informalfigure>')
 
         self.body.append('<%s>' % element)
@@ -819,7 +819,7 @@ class DocBookTranslator(nodes.NodeVisitor):
         self.body.append('</%s>' % element)
         # self.body.append('</para>')
 
-        if element == 'mediaobject' and not self.figure_tag_stack: # and not self.table_tag_stack:
+        if element == 'mediaobject' and not self.figure_tag_stack and not self.table_tag_stack:
             self.body.append('</informalfigure>')
 
     def depart_image(self, node):
